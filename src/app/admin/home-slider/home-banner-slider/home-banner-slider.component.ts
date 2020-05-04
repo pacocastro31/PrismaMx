72018d71fd076b1611fcd49ddc95efae2fb6ed19
Edgar Lopez <elopez@bluepeople.com>
2020-05-03 19:19:10 -0500

    gurda la info en la BD de banner slider

--- src/app/admin/home-slider/home-banner-slider/home-banner-slider.component.ts
+++ src/app/admin/home-slider/home-banner-slider/home-banner-slider.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 
+import { BannerInfo } from '../home-banner-info'
+import { SliderService } from '../slider.service'
+import { map } from 'rxjs/operators';
+
 @Component({
   selector: 'app-home-banner-slider',
   templateUrl: './home-banner-slider.component.html',
   styleUrls: ['./home-banner-slider.component.css']
 })
 export class HomeBannerSliderComponent implements OnInit {
+  contArr = []
+  cont = 1
+  banner: BannerInfo = new BannerInfo();
+  submitted = false;
 
-  constructor() { }
+  constructor(private sliderService: SliderService) { }
 
   ngOnInit(): void {
+    this.contArr.push(this.cont)
+  }
+
+  clickAgregar(){
+    this.contArr.push(this.cont + 1)
+  }
+
+  onSubmit() {
+    this.save();
+  }
+
+  save(){
+    this.submitted = true
+    this.sliderService.createBanner(this.banner)
   }
 
 }
