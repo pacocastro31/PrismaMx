b1d13a5f8c596319abea9b22d73bb2cda3f58715
Oscar Guevara <guevara1697@gmail.com>
2020-05-11 12:47:42 -0500

    Topbar con info de Firebase

--- src/app/topbar/topbar.component.ts
+++ src/app/topbar/topbar.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+import { ContactInfo } from '../admin/edit-info/contact-info'
+import { EditContactInfoService } from '../admin/edit-info/edit-contact-info.service'
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-topbar',
@@ -6,10 +10,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./topbar.component.css']
 })
 export class TopbarComponent implements OnInit {
+  @Input() contact: ContactInfo;
 
-  constructor() { }
+  contactInfo: ContactInfo = new ContactInfo();
+  info: any
+
+  constructor(private editContactInfoService: EditContactInfoService) { }
 
   ngOnInit(): void {
+    this.getContactInfo();
+  }
+
+  getContactInfo(){
+    this.editContactInfoService.getContactInfo().snapshotChanges().pipe(
+      map(changes=>
+        changes.map(c =>
+            ({key: c.payload.key, ...c.payload.val() })
+          )
+        )
+    ).subscribe(Cinfo => {
+      this.info = Cinfo;
+      console.log(this.info);
+    });
   }
 
 }
