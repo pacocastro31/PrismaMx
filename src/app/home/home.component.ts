dcd3cbcf882d7df92ecd27b60727d444ef2d9dab
Oscar Guevara <guevara1697@gmail.com>
2020-05-12 17:13:35 -0500

    Sección de servicios en el Home con info de Firebase

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
-import { BannerInfo } from '../admin/home-slider/home-banner-info'
-import { SliderService } from '../admin/home-slider/slider.service'
+import { BannerInfo } from '../admin/home-slider/home-banner-info';
+import { SliderService } from '../admin/home-slider/slider.service';
+
+import { ServicioParte1 } from '../admin/serviciosParte1/serviciosParte1';
+import { ServiciosParte1Service } from '../admin/serviciosParte1/servicios-parte1.service';
+
 import { map } from 'rxjs/operators';
 
 @Component({
@@ -12,14 +16,16 @@ import { map } from 'rxjs/operators';
 export class HomeComponent implements OnInit {
 
   sliders: any
+  servicios: any
 
-  constructor(private sliderService: SliderService) { }
+  constructor(private sliderService: SliderService, private servicio1Service: ServiciosParte1Service) { }
 
   ngOnInit(): void {
-    this.getInfo();
+    this.getInfoSliders();
+    this.getInfoServicios();
   }
 
-  getInfo(){
+  getInfoSliders(){
     this.sliderService.getSliderInfo().snapshotChanges().pipe(
       map(changes=>
         changes.map(c =>
@@ -28,6 +34,18 @@ export class HomeComponent implements OnInit {
         )
     ).subscribe(Cinfo => {
       this.sliders = Cinfo;
+    });
+  }
+
+  getInfoServicios(){
+    this.servicio1Service.getServicioInfo().snapshotChanges().pipe(
+      map(changes=>
+        changes.map(c =>
+            ({key: c.payload.key, ...c.payload.val() })
+          )
+        )
+    ).subscribe(Cinfo => {
+      this.servicios = Cinfo;
     });
   }
 
