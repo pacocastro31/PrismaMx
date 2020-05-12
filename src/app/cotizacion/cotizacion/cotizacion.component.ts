b74244030bfa3d4f973f2badef8b01656cc2d1ec
Noe <noe9704@hotmail.com>
2020-05-11 22:15:21 -0500

    PopUp con id

--- src/app/cotizacion/cotizacion/cotizacion.component.ts
+++ src/app/cotizacion/cotizacion/cotizacion.component.ts
@@ -34,7 +34,7 @@ export class CotizacionComponent implements OnInit {
     var result = '';
     var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     var charactersLength = characters.length;
-    for ( var i = 0; i < charactersLength; i++ ) {
+    for ( var i = 0; i < 40; i++ ) {
        result += characters.charAt(Math.floor(Math.random() * charactersLength));
     }
     console.log(result);
