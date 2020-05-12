cb2843d8abbb6e5afb0ea3b7ed4624dfcfe64af0
Oscar Guevara <guevara1697@gmail.com>
2020-05-12 12:02:21 -0500

    Agregar info de slider al component de home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { BannerInfo } from '../admin/home-slider/home-banner-info'
+import { SliderService } from '../admin/home-slider/slider.service'
+import { map } from 'rxjs/operators';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,9 +11,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  sliders: any
+
+  constructor(private sliderService: SliderService) { }
 
   ngOnInit(): void {
+    this.getInfo();
+  }
+
+  getInfo(){
+    this.sliderService.getSliderInfo().snapshotChanges().pipe(
+      map(changes=>
+        changes.map(c =>
+            ({key: c.payload.key, ...c.payload.val() })
+          )
+        )
+    ).subscribe(Cinfo => {
+      this.sliders = Cinfo;
+    });
   }
 
 }
