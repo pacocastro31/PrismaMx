ab741c4f4482b34a5da9208fb2d0e2d0bf053a23
Edgar Lopez <elopez@bluepeople.com>
2020-06-09 10:27:44 -0500

    login agregado

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  usuario: any
+  password: any
+
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(){
+    if(this.usuario != "admin" && this.password != "admin"){
+      alert("Usuario y/o Contraseña incorrecto")
+    } else{
+      localStorage.setItem("admin", this.usuario)
+      this.router.navigate(['admin']);
+    }
   }
 
 }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  admin: any
+  admin2 = false
+
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
+    this.admin = false
+    this.admin = localStorage.getItem('admin')
+    if(this.admin == "admin"){
+      this.admin2 = true
+    }
+  }
+
+  adminFunc(){
+    this.router.navigate(['admin']);
   }
 
 }
