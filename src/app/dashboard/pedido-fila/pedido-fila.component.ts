2fd66822d4206269ae2e49df775b55ecfc5ae8a4
Francisco Castro <pacocastro608@gmail.com>
2020-06-10 06:30:33 -0500

    entregados no se muestran

--- src/app/dashboard/pedido-fila/pedido-fila.component.ts
+++ src/app/dashboard/pedido-fila/pedido-fila.component.ts
@@ -26,11 +26,14 @@ export class PedidoFilaComponent implements OnInit {
   tieneImagen: boolean = false;
   imgFile: any = null
   subiendo: boolean = false;
-
+  entregado = true;
   nombreArch = ""
   fecha: any;
 
   ngOnInit(): void {
+    if(this.pedido.status == 'entregado'){
+      this.entregado = false
+    }
     this.fecha = String(this.pedido.dia) + "-" + String(this.pedido.mes) + "-" + String(this.pedido.ano)
     this.status = this.pedido.status;
     this.valorReal = this.pedido.precioReal
